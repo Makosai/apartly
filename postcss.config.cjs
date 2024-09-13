7acaaf87b9c87190f0bfb33e308f6ca0bb38ca51
Makosai <kristopher.ali.dev@gmail.com>
2024-09-13 15:52:19 -0400

    Setup the aliases and postcss configs.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,19 @@
-module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-	},
-}
+// eslint-disable-next-line
+const tailwindcss = require('tailwindcss');
+// eslint-disable-next-line
+const autoprefixer = require('autoprefixer');
+
+// eslint-disable-next-line
+const path = require('path');
+const tailwindConfig = path.join(__dirname, 'tailwind.config.ts');
+
+const config = {
+  plugins: [
+    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+    tailwindcss({ config: tailwindConfig }),
+    //But others, like autoprefixer, need to run after,
+    autoprefixer
+  ]
+};
+
+module.exports = config;
