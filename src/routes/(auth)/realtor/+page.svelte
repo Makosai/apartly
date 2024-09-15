5620baf1dcd3cebbcd31a81c7fc7bc01cbbc9dc6
Makosai <kristopher.ali.dev@gmail.com>
2024-09-15 04:28:03 -0400

    Layout a plan for the realtor page.

---
+++ src/routes/(auth)/realtor/+page.svelte
@@ -0,0 +1,20 @@
+<!-- 
+
+Realtor Page
+1. Put a floating + button on the bottom right corner of the screen.
+2. When the button is clicked, it should open a modal with a form to add a new property.
+3. The form should have the following fields:
+   - Property name
+   - Preview image
+   - Description
+   - Square footage
+   - Number of rooms
+   - $/month
+   - Location (lat/long), this will pop up a map to select the location.
+
+4. The page will list all properties the user owns. A simple filter.
+5. Each property should have a button to edit or delete the property.
+6. The edit button should open the same modal as the + button but with the property details filled in.
+7. The delete button should delete the property after a confirmation dialog.
+
+-->
