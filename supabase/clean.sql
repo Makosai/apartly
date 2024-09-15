d35de0729da263152c2fabf325412e207f57c327
Makosai <kristopher.ali.dev@gmail.com>
2024-09-14 21:37:06 -0400

    Finalize the database setup and cleanup before moving on to debugging and implementation.
    This change implements Postgis, account types, sets up functions/triggers to prevent unwanted side effects, secures users/apartments/apartment images, and automatically populates the users table on sign up which also have a backup where should that somehow go wrong...
    
    TODO: Then the user will be prompted again to fill in any missing information. Info they already have will populate the frontend by reading their metadata so they won't have to enter it again.

---
+++ supabase/clean.sql
@@ -0,0 +1,5 @@
+-- Clean up.
+drop policy if exists user_apartment_images_policy on storage.objects;
+-- drop table users, apartments;
+delete from storage.buckets where name = 'apartments_images';
+drop type acc_type;
