a81994408cc66f40e941716ca89fe04e6eb825ba
Makosai <kristopher.ali.dev@gmail.com>
2024-09-17 17:47:34 -0400

    Create a Listing component and update getPreviewUrl.
    Updated the getPreviewUrl function to include the appartment id.
    The listing component simplifies the /realtor route.

---
+++ src/components/formats/Listing.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { getPreviewUrl } from '$lib/info';
+
+	export let listing: ListingData;
+</script>
+
+<div class="card">
+	<img src={getPreviewUrl(listing.owner_id, listing.id)} alt={listing.title} />
+	<h2>{listing.title}</h2>
+	<p>{listing.description}</p>
+	<p>{listing.sq_footage} sq. ft.</p>
+	<p>{listing.rooms} rooms</p>
+	<p>${listing.monthly_price}/month</p>
+	<p>{listing.location_label}</p>
+	<div class="flex justify-between">
+		<a href="/realtor/edit/{listing.id}" class="btn-base-orange filled">Edit</a>
+		<button class="btn-base-red filled">Delete</button>
+	</div>
+</div>
--- src/lib/info.ts
+++ src/lib/info.ts
@@ -2,4 +2,4 @@ export const sitename = 'Apartly';
 export const siteurl = 'https://apartly.com';
 
 const imgs = 'https://wqyqougnnqwgqhjshpvz.supabase.co/storage/v1/object/public/apartments_images/';
-export const getPreviewUrl = (uid: string) => `${imgs}${uid}/preview`;
+export const getPreviewUrl = (uid: string, appt_id: number) => `${imgs}${uid}/${appt_id}/preview`;
