36b9665f6fbd9487f0d967cdf6808330bbf45717
Makosai <kristopher.ali.dev@gmail.com>
2024-09-18 02:00:37 -0400

    Implement editing listings.
    This completes U for CRUD. All CRUD operations implemented.
    
    Now to read for anons/users and add in filtering.

--- src/routes/(auth)/realtor/edit/[apartment_id]/+page.svelte
+++ src/routes/(auth)/realtor/edit/[apartment_id]/+page.svelte
@@ -5,12 +5,51 @@
 	import Map, { type Selection } from '$components/map/Map.svelte';
 
 	export let data;
-	const { supabase, user } = data;
+	const { supabase, user, apartment_id } = data;
 	const toast = getToastStore();
 
 	let submitting = false;
 	let selection: Selection | undefined;
 	let files: FileList | undefined;
+
+	let loaded_data: any | undefined = undefined;
+	let title: string;
+	let description: string;
+	let sq_footage: string;
+	let rooms: string;
+	let price_per_month: string;
+
+	loadApartment();
+
+	async function loadApartment() {
+		const { data, error } = await supabase
+			.from('apartments')
+			.select('title, description, sq_footage, rooms, monthly_price')
+			.eq('id', apartment_id)
+			.single();
+
+		if (error) {
+			toast.trigger({
+				message: error.message,
+				timeout: 2000,
+				background: 'variant-filled-warning'
+			});
+			return;
+		}
+
+		if (data) {
+			loaded_data = data;
+			title = data.title;
+			description = data.description;
+			sq_footage = data.sq_footage.toString();
+			rooms = data.rooms.toString();
+			price_per_month = data.monthly_price.toString();
+		}
+	}
+
+	function triggerToast(message: string, timeout = 2000, background = 'variant-filled-warning') {
+		toast.trigger({ message, timeout, background });
+	}
 
 	function validateForm(formData: FormData): boolean {
 		const title = formData.get('title') as string;
@@ -18,107 +57,52 @@
 		const sq_footage = formData.get('sq_footage') as string;
 		const rooms = formData.get('rooms') as string;
 		const price_per_month = formData.get('price_per_month') as string;
-		const location = formData.get('location') as string;
-		const location_label = formData.get('location_label') as string;
 
-		if (!files || files.length === 0) {
-			toast.trigger({
-				message: 'Please upload a preview image.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
-			return false;
-		}
+		// Files can be empty. It's optional to upload a new image.
+
+		// Text fields must be filled since we auto populate it.
 
 		if (!title) {
-			toast.trigger({
-				message: 'Please enter a title.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
+			triggerToast('Please enter a title.');
 			return false;
 		}
 
 		if (!description || description.length === 0) {
-			toast.trigger({
-				message: 'Please enter a description.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
+			triggerToast('Please enter a description.');
 			return false;
 		}
 
 		if (!sq_footage) {
-			toast.trigger({
-				message: 'Please enter the square footage.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
+			triggerToast('Please enter the square footage.');
 			return false;
 		}
 
 		if (isNaN(Number(sq_footage))) {
-			toast.trigger({
-				message: 'Square footage must be a number.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
+			triggerToast('Square footage must be a number.');
 			return false;
 		}
 
 		if (!rooms) {
-			toast.trigger({
-				message: 'Please enter the number of rooms.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
+			triggerToast('Please enter the number of rooms.');
 			return false;
 		}
 
 		if (isNaN(Number(rooms))) {
-			toast.trigger({
-				message: 'Number of rooms must be a number.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
+			triggerToast('Number of rooms must be a number.');
 			return false;
 		}
 
 		if (!price_per_month) {
-			toast.trigger({
-				message: 'Please enter the price per month.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
+			triggerToast('Please enter the price per month.');
 			return false;
 		}
 
 		if (isNaN(Number(price_per_month))) {
-			toast.trigger({
-				message: 'Price per month must be a number.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
+			triggerToast('Price per month must be a number.');
 			return false;
 		}
 
-		if (!location) {
-			toast.trigger({
-				message: 'Please select a location.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
-			return false;
-		}
-
-		if (!location_label) {
-			toast.trigger({
-				message: 'Please select a location.',
-				timeout: 2000,
-				background: 'variant-filled-warning'
-			});
-			return false;
-		}
+		// Location is optional. We don't populate it.
 
 		return true;
 	}
@@ -140,26 +124,14 @@
 			const dataEntries = Object.fromEntries(formData.entries());
 
 			if (!validateForm(formData)) return;
-			if (!files || files.length <= 0) return; // Handled in validateForm, this just shuts up JS.
 
-			const file = files.item(0);
-			if (file === null) return;
-			const fileType = file.type.split('/')[1];
-
-			if (fileType !== 'png' && fileType !== 'jpeg' && fileType !== 'jpg') {
-				toast.trigger({
-					message: 'Preview image must be a PNG or JPEG file.',
-					timeout: 2000,
-					background: 'variant-filled-warning'
-				});
-				return;
-			}
-
-			let apartment_id = -1;
 			{
-				const { data, error } = await supabase
+				// To be absolutely efficient, we should check if the data is different before updating.
+				// But for the sake of simplicity, we'll just update it.
+				// It's simple, but really it's overkill because we're not scaling this test app.
+				const { error } = await supabase
 					.from('apartments')
-					.insert({
+					.update({
 						owner_id: user.id,
 						title: dataEntries.title as string,
 						description: dataEntries.description as string,
@@ -169,7 +141,7 @@
 						location: dataEntries.location as string,
 						location_label: dataEntries.location_label as string
 					})
-					.select();
+					.eq('id', apartment_id);
 
 				if (error) {
 					toast.trigger({
@@ -179,26 +151,37 @@
 					});
 					return;
 				}
-
-				if (data && data.length > 0) {
-					apartment_id = data[0].id;
-				}
 			}
 
-			const { error } = await supabase.storage
-				.from('apartments_images')
-				.upload(`/${user.id}/${apartment_id}/preview`, file, {
-					cacheControl: '3600',
-					upsert: true
-				});
+			if (files && files.length > 0) {
+				const file = files.item(0);
+				if (file === null) return;
+				const fileType = file.type.split('/')[1];
 
-			if (error) {
-				toast.trigger({
-					message: error.message,
-					timeout: 2000,
-					background: 'variant-filled-warning'
-				});
-				return;
+				if (fileType !== 'png' && fileType !== 'jpeg' && fileType !== 'jpg') {
+					toast.trigger({
+						message: 'Preview image must be a PNG or JPEG file.',
+						timeout: 2000,
+						background: 'variant-filled-warning'
+					});
+					return;
+				}
+
+				const { error } = await supabase.storage
+					.from('apartments_images')
+					.upload(`/${user.id}/${apartment_id}/preview`, file, {
+						cacheControl: '3600',
+						upsert: true
+					});
+
+				if (error) {
+					toast.trigger({
+						message: error.message,
+						timeout: 2000,
+						background: 'variant-filled-warning'
+					});
+					return;
+				}
 			}
 
 			toast.trigger({
@@ -217,22 +200,19 @@
 </script>
 
 <PageContainer>
-	<h1 class="title">Add Listing</h1>
-	<p class="subtitle">Add a new listing.</p>
+	<h1 class="title">Edit Listing</h1>
+	<p class="subtitle">Change the information for Apartment ID#{apartment_id}</p>
 
 	<div class="flex justify-center items-center">
-		<form
-			class="form-container"
-			on:submit|preventDefault={submitForm}
-		>
+		<form class="form-container" on:submit|preventDefault={submitForm}>
 			<div class="form-group">
 				<label for="title">Title</label>
-				<input type="text" id="title" name="title" required />
+				<input type="text" id="title" name="title" bind:value={title} required />
 			</div>
 
 			<div class="form-group md:col-span-2">
 				<label for="description">Description</label>
-				<textarea id="description" name="description" required></textarea>
+				<textarea id="description" name="description" bind:value={description} required></textarea>
 			</div>
 
 			<div class="form-group">
@@ -245,6 +225,7 @@
 					step="1"
 					required
 					pattern="[0-9]+"
+					bind:value={sq_footage}
 				/> sq/ft
 			</div>
 
@@ -258,6 +239,7 @@
 					step="1"
 					required
 					pattern="[0-9]+"
+					bind:value={rooms}
 				/> rooms
 			</div>
 
@@ -272,11 +254,13 @@
 					required
 					pattern="/(?:^[1-9]([0-9]+)?(?:\.[0-9]\{(1,
 					2)})?$)|(?:^(?:0)$)|(?:^[0-9]\.[0-9](?:[0-9])?$)/"
+					bind:value={price_per_month}
 				/> USD
 			</div>
 
 			<div class="form-group">
 				<label for="preview-image-url">Preview Image</label>
+				<p>It's optional to upload an image to replace the old one.</p>
 				{#if files && files.length > 0}
 					<p>
 						File name: {files[0].name}
@@ -298,7 +282,11 @@
 
 			<div class="form-group md:col-span-2">
 				<label for="location">Location</label>
-				<p>Search for a location on the map below.</p>
+				<p>
+					Search for a location on the map below <strong
+						>if you want to change your old location</strong
+					>.
+				</p>
 				<p>{selection?.label ?? ''}</p>
 				<div class="w-full h-[300px]">
 					<Map bind:selection />
@@ -307,19 +295,9 @@
 
 			<div class="form-group">
 				<button type="submit" class="btn-base-orange filled pill">
-					{submitting ? 'Adding Your Listing...' : 'Finish Posting Listing'}
+					{submitting ? 'Updating Your Listing...' : 'Update Your Listing'}
 				</button>
 			</div>
 		</form>
 	</div>
 </PageContainer>
-
-<style lang="postcss">
-	.form-container {
-		@apply grid w-full grid-cols-1 md:grid-cols-2 md:grid-rows-none gap-x-10 gap-y-0 	md:gap-y-4;
-	}
-
-	.form-container textarea {
-		@apply w-full;
-	}
-</style>
---
+++ src/routes/(auth)/realtor/edit/[apartment_id]/+page.ts
@@ -0,0 +1,5 @@
+export const load = ({ params }) => {
+	return {
+		apartment_id: params.apartment_id
+	};
+};
