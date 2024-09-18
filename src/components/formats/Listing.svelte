d30f5fa05edab118e8d6c0d295d1152e63243b0c
Makosai <kristopher.ali.dev@gmail.com>
2024-09-17 21:01:27 -0400

    Add a btn-error style and improve listing overall.
    This preps us for Update and Delete.

--- src/components/formats/Listing.svelte
+++ src/components/formats/Listing.svelte
@@ -1,19 +1,39 @@
 <script lang="ts">
 	import { getPreviewUrl } from '$lib/info';
+	import Icon from '@iconify/svelte';
 
 	export let listing: ListingData;
 </script>
 
 <div class="card">
-	<img src={getPreviewUrl(listing.owner_id, listing.id)} alt={listing.title} />
-	<h2>{listing.title}</h2>
-	<p>{listing.description}</p>
-	<p>{listing.sq_footage} sq. ft.</p>
-	<p>{listing.rooms} rooms</p>
-	<p>${listing.monthly_price}/month</p>
-	<p>{listing.location_label}</p>
-	<div class="flex justify-between">
-		<a href="/realtor/edit/{listing.id}" class="btn-base-orange filled">Edit</a>
-		<button class="btn-base-red filled">Delete</button>
+	<div class="flex flex-col">
+		<img src={getPreviewUrl(listing.owner_id, listing.id)} alt={listing.title} />
+		<h3>{listing.title}</h3>
+		<p>{listing.description}</p>
+		<p>{listing.sq_footage} sq. ft.</p>
+		<p>{listing.rooms} rooms</p>
+		<p>${listing.monthly_price}/month</p>
+		<p>{listing.location_label}</p>
+	</div>
+	<div class="flex justify-end gap-4 mt-4">
+		<button class="btn-base-orange filled pill basis-0 grow">Edit</button>
+		<button class="btn-error pill basis-0">
+			<Icon icon="mdi:trash" />
+		</button>
 	</div>
 </div>
+
+<style lang="postcss">
+	p {
+		@apply text-sm;
+	}
+	img {
+		@apply rounded-2xl mb-4;
+    @apply w-[415px] h-[280px];
+	}
+	.card {
+		@apply bg-white shadow-md rounded-3xl p-4;
+		@apply flex flex-col justify-between;
+		@apply max-w-md w-full mx-auto;
+	}
+</style>
